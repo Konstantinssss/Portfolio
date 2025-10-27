0ae50d2534c4dc3f25e025a84667ec1b248b855b
Konstantin <mayorkos78@gmail.com>
2025-10-27 17:35:39 +0300

    first commit

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 @pytest.fixture(scope='session')
 def browser():
     driver = webdriver.Chrome()
-    driver.set_window_size(1000, 2500)
+    driver.set_window_size(2500, 1000)
     yield driver
     driver.quit()
 
