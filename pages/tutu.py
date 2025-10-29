d7ba56cfd24126c43a7afe209285c3ef514b9ae5
Konstantin <mayorkos78@gmail.com>
2025-10-29 19:10:58 +0300

    first commit

--- pages/tutu.py
+++ pages/tutu.py
@@ -9,5 +9,5 @@ class Tutu(BasePage):
         self.base_url = 'https://www.tutu.ru/'
         super().__init__(driver, self.base_url)
 
-        self.btn_burger_menu = WebElement(driver, '#__next > header > div > div > ul > li.listStyleTypeNone_2c663590.marginRight450_36bb7eca > ul > li:nth-child(1) > a')
+        self.btn_burger_menu = WebElement(driver, 'span.richIcon_87ec6c5e.size_200_87ec6c5e.shapeNoShape_87ec6c5e > i')
         self.btn_main = WebElement(driver, 'div > a')
